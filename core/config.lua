29c2affb6458787ebb382cef6ba10666d41ccf24
dealblox <dealblox.suporte@gmail.com>
2026-08-30 23:20:48 -0300

    Add global configuration for DEAL BLOX

--- core/config.lua
+++ core/config.lua
@@ -1,1 +1,29 @@
+-- DEAL BLOX
+-- Configurações globais
 
+local Config = {}
+
+Config.Name = "DEAL BLOX"
+Config.Version = "2.0 DEV"
+
+Config.Site = "https://dealblox.com.br"
+Config.Discord = "discord.gg/rPFN7BMC5k"
+
+Config.Logo = "rbxassetid://92760392968513"
+
+Config.Colors = {
+	Blue = Color3.fromRGB(0, 125, 255),
+	BlueNeon = Color3.fromRGB(0, 180, 255),
+
+	Red = Color3.fromRGB(220, 25, 45),
+	RedNeon = Color3.fromRGB(255, 45, 70),
+
+	Background = Color3.fromRGB(7, 10, 17),
+	Panel = Color3.fromRGB(11, 15, 24),
+	Panel2 = Color3.fromRGB(18, 23, 35),
+
+	Text = Color3.fromRGB(245, 248, 255),
+	SubText = Color3.fromRGB(165, 175, 195)
+}
+
+return Config
