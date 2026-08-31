04fad4884dd3d464edd4dbd1fa565a9de22a89c4
dealblox <dealblox.suporte@gmail.com>
2026-08-30 22:43:56 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,3 @@
 -- DEAL BLOX V2 - LOADER
 
-loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/dealblox/dealbloxscript/refs/heads/main/main.lua"
-))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dealblox/dealbloxscript/refs/heads/principal/main.lua"))()
